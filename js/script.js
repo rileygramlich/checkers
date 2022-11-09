187d117fb6028f485531495823eaab6154ea6567
rileygramlich <rileygramlich@gmail.com>
2022-11-09 13:47:45 -0700

    add comments

--- js/script.js
+++ js/script.js
@@ -25,11 +25,11 @@ let board = [
     [red.piece, null, red.piece, null, red.piece, null, red.piece, null]
 ]
 
-
 let redEndSquares = [1, 3, 5, 7]
 let blackEndSquares = [0, 2, 4, 6]
 
 let turn = 1
+
 let gameOn = true 
 
 let xOrigin = null
@@ -48,9 +48,6 @@ let winner = null
 
 let clicks = 1
 
-let xDestinationOptions = []
-let yDestinationOptions = []
-
 
 /*----- cached element references -----*/
 let boardEl = document.getElementById('board')
@@ -63,9 +60,8 @@ let blacksEl = document.getElementById('#blacks')
 
 
 /*----- event listeners -----*/
-buildBoard()
-init()
 
+// Listens to which square was selected and calls function on click according to whose turn it is:
 boardEls.addEventListener('click', function(event) {
     console.log('clicked')
     event.preventDefault
@@ -87,35 +83,145 @@ boardEls.addEventListener('click', function(event) {
     }
 })
 
+// Listens for a click on the reset button and initilizes the board to start over:
 resetEl.addEventListener('click', function(event) {
     console.log('resetting')
     init()
 })
 
+
 /*----- functions -----*/
+
+// Builds an 8x8 chess board elements for DOM manipulation with a nested for loop:
+function buildBoard() {
+    board.forEach(function(row, y) {
+        let rowEl = document.createElement('div')
+        rowEl.classList.add('row')
+        rowEl.id = `${y}`
+            if (y % 2 === 0) {
+                row.forEach(function(cell, x) {
+                    let cellEl = document.createElement('div')
+                    if (x % 2 !== 0) {
+                        cellEl.classList.add('dark', 'square')
+                    } else {
+                        cellEl.classList.add('light', 'square')
+                    }
+                    cellEl.id = `${x}-${y}`
+                    rowEl.append(cellEl)
+                })
+            } else {
+                row.forEach(function(cell, x) {
+                    let cellEl = document.createElement('div')
+                    if (x % 2 === 0) {
+                        cellEl.classList.add('dark', 'square')
+                    } else {
+                        cellEl.classList.add('light', 'square')
+                    }
+                    cellEl.id = `${x}-${y}`
+                    rowEl.append(cellEl)
+                })
+            }
+            console.log(rowEl)
+            boardEl.append(rowEl)
+    })
+}
+
+// Initializes the game to the starting conditions:
+function init() {
+    console.log('initializing')
+    red = {
+        piece : 'redPiece',
+        king : 'redKing',
+        totalPieces : 12,
+    }
+    
+    black = {
+        piece : 'blackPiece',
+        king : 'blackKing',
+        totalPieces : 12,
+    }
+    
+    turn = 1
+
+    board = [
+        [null, black.piece, null, black.piece, null, black.piece, null, black.piece],
+        [black.piece, null, black.piece, null, black.piece, null, black.piece, null],
+        [null, black.piece, null, black.piece, null, black.piece, null, black.piece],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [red.piece, null, red.piece, null, red.piece, null, red.piece, null],
+        [null, red.piece, null, red.piece, null, red.piece, null, red.piece],
+        [red.piece, null, red.piece, null, red.piece, null, red.piece, null]
+    ]
+    render()
+}
+
+// Renders the 2-dimensional array elements to the webpage board and whose turn it is:
+function render() {
+    console.log('rendering:')
+    console.log(board)
+    board.forEach((row, y) => {
+        row.forEach((cell, x) => {
+            let cellEl = document.getElementById(`${x}-${y}`)
+            cellEl.classList.remove('redPiece', 'blackPiece', 'null', 'blackKing', 'redKing', 'selected')
+            cellEl.classList.add(`${board[y][x]}`)
+        })
+    })
+    turn === 1 ? moveDisplayEl.innerText = `Red's move` : moveDisplayEl.innerText = `Black's move`
+    // redsEl.innerText = `${13 - black.totalPieces}`
+    // blacksEl.innerText = `${13 - red.totalPieces}`
+}
+
+// Runs necessary functions to make move for Red:
 function redMakeMove() {
     if (clicks === 1) {
         getRedOriginSquare()
     } else if (clicks === 2) {
-        getDestinationSquare()
+        moveToDestinationSquare()
     }
 }
 
+// Runs necessary functions to make move for Black:
+function blackMakeMove() {
+    if (clicks === 1) {
+        getBlackOriginSquare()
+    } else if (clicks === 2) {
+        moveToDestinationSquare()
+    }
+}
+
+// Checks if selected piece is a Red piece or a Red king to secure origin square coords:
 function getRedOriginSquare() {
     if (board[ySelected][xSelected] === red.piece || board[ySelected][xSelected] === red.king) {
-        xOrigin = xSelected
-        yOrigin = ySelected
-        console.log('Secured origin square')
-        clicks = 2
-        console.log(clicks)
-        selected.classList.add('selected')
+        getOriginSquare()
     } else {
         console.log('Please select a red piece')
         return
     }
 }
 
-function getDestinationSquare() {
+// Checks if selected piece is a Black piece or a Black king to secure origin square coords:
+function getBlackOriginSquare() {
+    if (board[ySelected][xSelected] === black.piece || board[ySelected][xSelected] === black.king) {
+        getOriginSquare()
+    } else {
+        console.log('Please select a black piece')
+        return
+    }
+}
+
+// Secures the orgin square coordinates
+function getOriginSquare() {
+    xOrigin = xSelected
+    yOrigin = ySelected
+    console.log('Secured origin square')
+    clicks = 2
+    console.log(clicks)
+    selected.classList.add('selected')
+}
+
+// If possible, makes the move to the destination square and renders it:
+function moveToDestinationSquare() {
     console.log(`Origin coords: ${xOrigin}, ${yOrigin}`)
     console.log(`Selected coords: ${xSelected}, ${ySelected}`)
     checkIfJumpExists()
@@ -139,25 +245,7 @@ function getDestinationSquare() {
     }
 }
 
-function checkIfMoveExists() {
-    console.log('checking if move exists')
-    if (board[yOrigin][xOrigin] === red.king || board[yOrigin][xOrigin] === black.king) {
-        if (yOrigin - ySelected === 1 && Math.abs(xOrigin - xSelected) === 1) {
-            moveExists = true
-        } else if (yOrigin - ySelected === -1 && Math.abs(xOrigin - xSelected) === 1) {
-            moveExists = true
-        }
-    } else if (turn === 1) {
-        if (yOrigin - ySelected === 1 && Math.abs(xOrigin - xSelected) === 1) {
-            moveExists = true
-        }
-    } else {
-        if (yOrigin - ySelected === -1 && Math.abs(xOrigin - xSelected) === 1) {
-            moveExists = true
-        }
-    }
-}
-
+// Checks if it's possible to jump to the selected square:
 function checkIfJumpExists() {
     console.log('checking if jump exists')
     if (turn === 1) {
@@ -187,6 +275,27 @@ function checkIfJumpExists() {
     }
 }
 
+// Checks if it's possible to move to the selected square:
+function checkIfMoveExists() {
+    console.log('checking if move exists')
+    if (board[yOrigin][xOrigin] === red.king || board[yOrigin][xOrigin] === black.king) {
+        if (yOrigin - ySelected === 1 && Math.abs(xOrigin - xSelected) === 1) {
+            moveExists = true
+        } else if (yOrigin - ySelected === -1 && Math.abs(xOrigin - xSelected) === 1) {
+            moveExists = true
+        }
+    } else if (turn === 1) {
+        if (yOrigin - ySelected === 1 && Math.abs(xOrigin - xSelected) === 1) {
+            moveExists = true
+        }
+    } else {
+        if (yOrigin - ySelected === -1 && Math.abs(xOrigin - xSelected) === 1) {
+            moveExists = true
+        }
+    }
+}
+
+// Changes the board values to reflect the executred move:
 function move() {
     xDestination = xSelected
     yDestination = ySelected
@@ -197,28 +306,25 @@ function move() {
     canJump = false
 }
 
-function blackMakeMove() {
-    if (clicks === 1) {
-        getBlackOriginSquare()
-    } else if (clicks === 2) {
-        getDestinationSquare()
-}
-}
-
-function getBlackOriginSquare() {
-    if (board[ySelected][xSelected] === black.piece || board[ySelected][xSelected] === black.king) {
-        xOrigin = xSelected
-        yOrigin = ySelected
-        console.log('Secured origin square')
-        clicks = 2
-        console.log(clicks)
-        selected.classList.add('selected')
+// Promotes the piece to a king if it hits one of its end squares:
+function checkForKing() {
+    console.log('checking to make king')
+    if (turn === 1) {
+        console.log(redEndSquares)
+        if (yDestination === 0 && redEndSquares.includes(xDestination)) {
+            console.log(board[yDestination][xDestination])
+            board[yDestination][xDestination] = red.king
+        }
     } else {
-        console.log('Please select a black piece')
-        return
+        console.log(blackEndSquares)
+        if (yDestination === 7 && blackEndSquares.includes(xDestination)) {
+            console.log(board[yDestination][xDestination])
+            board[yDestination][xDestination] = black.king
+        }
     }
 }
 
+// Checks if a piece was captured and updates total pieces and removes the piece, if it was, then renders it to the page:
 function checkForCapture() {
     console.log('checking for capture')
     if (turn === 1) {
@@ -274,24 +380,8 @@ function checkForCapture() {
     render()
     checkForWinner()
 }
- 
-function checkForKing() {
-    console.log('checking to make king')
-    if (turn === 1) {
-        console.log(redEndSquares)
-        if (yDestination === 0 && redEndSquares.includes(xDestination)) {
-            console.log(board[yDestination][xDestination])
-            board[yDestination][xDestination] = red.king
-        }
-    } else {
-        console.log(blackEndSquares)
-        if (yDestination === 7 && blackEndSquares.includes(xDestination)) {
-            console.log(board[yDestination][xDestination])
-            board[yDestination][xDestination] = black.king
-        }
-    }
-}
 
+// If there's a winner, displays winner and turns the game off waiting for reset:
 function checkForWinner() {
     if (red.totalPieces === 0) {
         console.log(`Black Wins`)
@@ -306,82 +396,7 @@ function checkForWinner() {
     }
 }
 
-// Building a chess board with js:
-function buildBoard() {
-    board.forEach(function(row, y) {
-        let rowEl = document.createElement('div')
-        rowEl.classList.add('row')
-        rowEl.id = `${y}`
-            if (y % 2 === 0) {
-                row.forEach(function(cell, x) {
-                    let cellEl = document.createElement('div')
-                    if (x % 2 !== 0) {
-                        cellEl.classList.add('dark', 'square')
-                    } else {
-                        cellEl.classList.add('light', 'square')
-                    }
-                    cellEl.id = `${x}-${y}`
-                    rowEl.append(cellEl)
-                })
-            } else {
-                row.forEach(function(cell, x) {
-                    let cellEl = document.createElement('div')
-                    if (x % 2 === 0) {
-                        cellEl.classList.add('dark', 'square')
-                    } else {
-                        cellEl.classList.add('light', 'square')
-                    }
-                    cellEl.id = `${x}-${y}`
-                    rowEl.append(cellEl)
-                })
-            }
-            console.log(rowEl)
-            boardEl.append(rowEl)
-    })
-}
 
-function render() {
-    // To render the board array to the boardEl we will do another nested array
-    console.log('rendering:')
-    console.log(board)
-    board.forEach((row, y) => {
-        row.forEach((cell, x) => {
-            let cellEl = document.getElementById(`${x}-${y}`)
-            cellEl.classList.remove('redPiece', 'blackPiece', 'null', 'blackKing', 'redKing', 'selected')
-            cellEl.classList.add(`${board[y][x]}`)
-        })
-    })
-    turn === 1 ? moveDisplayEl.innerText = `Red's move` : moveDisplayEl.innerText = `Black's move`
-    // redsEl.innerText = `${13 - black.totalPieces}`
-    // blacksEl.innerText = `${13 - red.totalPieces}`
-}
-
-
-function init() {
-    console.log('initializing')
-    red = {
-        piece : 'redPiece',
-        king : 'redKing',
-        totalPieces : 12,
-    }
-    
-    black = {
-        piece : 'blackPiece',
-        king : 'blackKing',
-        totalPieces : 12,
-    }
-    
-    turn = 1
-
-    board = [
-        [null, black.piece, null, black.piece, null, black.piece, null, black.piece],
-        [black.piece, null, red.king, null, black.piece, null, black.piece, null],
-        [null, black.piece, null, black.piece, null, black.piece, null, black.piece],
-        [null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null],
-        [red.piece, null, red.piece, null, red.piece, null, red.piece, null],
-        [null, black.king, null, red.piece, null, black.king, null, red.piece],
-        [red.piece, null, red.piece, null, red.piece, null, red.piece, null]
-    ]
-    render()
-}
+/*----- game -----*/
+buildBoard()
+init()
